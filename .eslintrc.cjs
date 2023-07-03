0f0191d32f649e06485cd7536557839ff9597ae6
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-07-03 16:00:28 -0600

    :wrench: Add eslint and prettier dev config

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    semi: ['error', 'never'],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    'import/extensions': [
+      'off',
+    ],
+    'no-console': 'off',
+    'no-unused-vars': ['off'],
+  },
+}

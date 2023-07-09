47ca920ab468c2c7fe7e192e3188364873e62409
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-07-09 16:51:24 -0600

    :sparkles: Add database first models

---
+++ models/property.js
@@ -0,0 +1,53 @@
+import { DataTypes } from 'sequelize'
+import db from '../config/db.js'
+
+const Property = db.define('properties', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+  },
+  rooms: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  parking: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  wc: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  street: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+  lat: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lng: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  published: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+})
+
+export default Property

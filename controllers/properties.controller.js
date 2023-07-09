7b45ab2cd9baff6dd3ecd667a2140b336ef8b2f0
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-07-09 16:52:28 -0600

    :sparkles: Add show all info for create property form

--- controllers/properties.controller.js
+++ controllers/properties.controller.js
@@ -1,13 +1,23 @@
 import { request, response } from 'express'
 
+import Category from '../models/category.js'
+import Price from '../models/price.js'
+
 export const admin = (req = request, res = response) => {
   res.render('properties/admin', {
     page: 'Mis propiedades',
   })
 }
 
-export const createProperty = (req = request, res = response) => {
+export const createProperty = async (req = request, res = response) => {
+  const [categories, prices] = await Promise.all([
+    Category.findAll(),
+    Price.findAll(),
+  ])
+
   res.render('properties/create', {
     page: 'Crear propiedad',
+    categories,
+    prices,
   })
 }

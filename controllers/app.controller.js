43baaadb39dd59a380fd4c935bfff8c01b8a44d6
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-07-17 15:44:47 -0600

    :art: Add home page designed

--- controllers/app.controller.js
+++ controllers/app.controller.js
@@ -1,8 +1,42 @@
 import { request, response } from 'express'
+import models from '../models/index.js'
+
+const { Price, Category, Property } = models
 
 export const home = async (req = request, res = response) => {
+  const [categories, prices, houses, apartments] = await Promise.all([
+    Category.findAll({ raw: true }),
+    Price.findAll({ raw: true }), /* Cuando usamos raw en true lo que hace es que
+    limpia los resultados y unicamente nos trae la información del objeto, como el lean
+    en mongodb */
+    Property.findAll({
+      limit: 3,
+      where: {
+        categoryId: 1,
+      },
+      include: [
+        { model: Price, as: 'price' },
+      ],
+      order: [['createdAt', 'DESC']],
+    }),
+    Property.findAll({
+      limit: 3,
+      where: {
+        categoryId: 2,
+      },
+      include: [
+        { model: Price, as: 'price' },
+      ],
+      order: [['createdAt', 'DESC']],
+    }),
+  ])
+
   res.render('home', {
     page: 'Home',
+    categories,
+    prices,
+    houses,
+    apartments,
   })
 }
 
--- src/js/home-map.js
+++ src/js/home-map.js
@@ -1,3 +1,4 @@
+/* eslint-disable new-cap */
 /* eslint-disable no-undef */
 /* eslint-disable func-names */
 /* eslint-disable wrap-iife */
@@ -7,7 +8,80 @@
   const lng = -102.304627
   const mapa = L.map('home-map').setView([lat, lng], 15)
 
+  const markers = new L.FeatureGroup().addTo(mapa)
+  let properties = []
+
+  const filters = {
+    category: '',
+    price: '',
+  }
+
+  const categoriesSelect = document.querySelector('#categories')
+  const pricesSelect = document.querySelector('#prices')
+
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(mapa)
+
+  const showProperties = (propertiesArg) => {
+    // Clean prev markers
+    markers.clearLayers()
+
+    propertiesArg.forEach((property) => {
+      // Add the pins
+      const marker = new L.marker([property?.lat, property?.lng], {
+        autoPan: true,
+      })
+        .addTo(mapa)
+        .bindPopup(`
+          <p class="text-indigo-600 font-bold">${property?.category?.name}</p>
+          <h1 class="text-xl font-extrabold uppercase my-2">${property?.title}</h1>
+          <img src="/uploads/${property?.image}" alt="Propiedad: ${property?.title}" />
+          <p class="text-gray-600 font-bold">${property?.price?.name}</p>
+          <a href="/property/${property?.id}" class="bg-indigo-600 block p-2 text-center font-bold uppercase text-white">Ver propiedad</a>
+        `)
+
+      markers.addLayer(marker)
+    })
+  }
+
+  const filterProperties = () => {
+    const resultFilters = properties
+      .filter((property) => (
+        filters.category
+          ? property.categoryId === filters.category
+          : property
+      ))
+      .filter((property) => (
+        filters.price
+          ? property.priceId === filters.price
+          : property
+      ))
+
+    showProperties(resultFilters)
+  }
+
+  const getProperties = async () => {
+    try {
+      const url = '/api/properties'
+      const response = await fetch(url)
+      properties = await response.json()
+
+      showProperties(properties)
+    } catch (error) {
+      console.log({ error })
+    }
+  }
+
+  categoriesSelect.addEventListener('change', (e) => {
+    filters.category = +e.target.value
+    filterProperties()
+  })
+
+  pricesSelect.addEventListener('change', (e) => {
+    filters.price = +e.target.value
+    filterProperties()
+  })
+
+  getProperties()
 })()
